b2c4cdf35a032dd0eb22eea580d4f2244bf61182
DOGTT <bolitt@hotmail.com>
2017-06-25 10:36:48 +0800

    f

---
+++ config/default.js
@@ -0,0 +1,13 @@
+module.exports = {
+    //basic config
+    name:'my Site',
+    port:3000,
+    //the basic projects path : ./public/project+pathHtmlFind[]
+    pathProjectsBasic:'projects',
+    pathProjectsFind:[
+        'test'
+    ],
+    pathProjectsImg:'projects_img',
+    pathProjectsInfo:'projects_info.json'
+
+}
